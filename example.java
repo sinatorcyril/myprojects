56dad7dead7f2121a228f419e5cc380cc543948e
Assibong Cyril <sinator_cyril.com>
2017-01-02 20:30:58 +0200

    a java class

---
+++ example.java
@@ -0,0 +1,18 @@
+public class example {
+
+	public static void main(String[] args) {
+		
+		
+		int a ,b,c;
+		
+		a=20; 
+		
+		b= 2;
+		
+		c= a+b;
+		
+		System.out.println("the value of c is :"+ c);
+
+	}
+
+}
